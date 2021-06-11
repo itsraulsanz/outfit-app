a0acaf3a14fb0636c8b78dd9b9d095c69881ce0d
Raul Sanz <luar_znas@hotmail.com>
2021-06-11 19:13:29 +0100

    Cleaned and formated the code

--- outfit-app/public/js/post.js
+++ outfit-app/public/js/post.js
@@ -5,15 +5,15 @@ async function createOutfitFormHandler(event) {
   const price = document.querySelector("#price").value.trim();
   const brand = document.querySelector("#brand").value.trim();
   const location = document.querySelector("#location").value.trim();
-    
-  const occasionElement = document.querySelector('#occasion');
-  const occasion = occasionElement.value; 
 
-  const colourElement = document.querySelector('#colour');
-  const colour = colourElement.value; 
+  const occasionElement = document.querySelector("#occasion");
+  const occasion = occasionElement.value;
 
-  const genderElement = document.querySelector('#gender');
-  const gender = genderElement.value; 
+  const colourElement = document.querySelector("#colour");
+  const colour = colourElement.value;
+
+  const genderElement = document.querySelector("#gender");
+  const gender = genderElement.value;
 
   const notes = document.querySelector("#notes").value.trim();
   const image = document.querySelector("#imageLink").value;
@@ -21,7 +21,6 @@ async function createOutfitFormHandler(event) {
   console.log("occasion", occasion);
   console.log("colour", colour);
   console.log("gender", gender);
-
 
   if (
     outfitName &&
@@ -62,22 +61,23 @@ document
   .querySelector(".new-outfit-form")
   .addEventListener("submit", createOutfitFormHandler);
 
-const imageNode =  document.querySelector("#image");
+const imageNode = document.querySelector("#image");
 
 const uploadFile = () => {
-  const file =  document.querySelector("#image").files[0];
+  const file = document.querySelector("#image").files[0];
   let formData = new FormData();
-     
+
   formData.append("picture", file);
-  fetch('/api/outfits/upload', {
-     method: "POST",
-     body: formData
-   }).then((resp) => resp.json())
-     .then((response) =>{
+  fetch("/api/outfits/upload", {
+    method: "POST",
+    body: formData,
+  })
+    .then((resp) => resp.json())
+    .then((response) => {
       console.log(response);
-      const imgContent =  document.querySelector("#imageLink");
+      const imgContent = document.querySelector("#imageLink");
       imgContent.value = response.resp.eager[0].secure_url;
-     });
-}
+    });
+};
 
 imageNode.addEventListener("change", uploadFile, false);
